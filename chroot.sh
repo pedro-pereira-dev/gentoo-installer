8d20d60e798285ffca3cec71719dbd8c1686524c
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2025-05-09 17:21:37 +0100

    artix installation
    new test

--- chroot.sh
+++ chroot.sh
@@ -1,42 +1,23 @@
 #!/bin/bash
 
-emerge-webrsync
-emerge --ask=n --backtrack=30 --deep --newuse --update --verbose --with-bdeps=y @world
-
-sed --in-place 's/keymap="us"/keymap="'"${KEYMAP}"'"/g' /etc/conf.d/keymaps
-ln --force --symbolic "/usr/share/zoneinfo/${TIMEZONE}" /etc/localtime
 hwclock --systohc
+ln --force --symbolic "/usr/share/zoneinfo/${TIMEZONE}" /etc/localtime
 sed --in-place 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
-eselect locale set 4
-env-update && source /etc/profile
 
-echo 'sys-kernel/installkernel dracut grub' >>/etc/portage/package.use
-echo 'sys-kernel/linux-firmware @BINARY-REDISTRIBUTABLE' >>/etc/portage/package.license
-emerge --ask=n sys-kernel/installkernel sys-kernel/linux-firmware
-
-mkdir --parents /boot/efi
 sed --in-place 's/.*GRUB_CMDLINE_LINUX_DEFAULT.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/g' /etc/default/grub
 sed --in-place 's/.*GRUB_TIMEOUT.*/GRUB_TIMEOUT=1/g' /etc/default/grub
-grub-install --bootloader-id='Gentoo Grub' --efi-directory=/boot/efi --target=x86_64-efi
-emerge --ask=n sys-kernel/gentoo-kernel-bin
+grub-install --efi-directory=/boot --removable --target=x86_64-efi
+grub-mkconfig -o /boot/grub/grub.cfg
 
-cat <<EOF >/etc/fstab
-# <fs> <mountpoint> <type> <opts> <dump> <pass>
-${DISK_DEVICE}${DISK_PARTITION_SEPARATOR}1 /boot vfat defaults,nodev,nosuid,noexec,fmask=0177,dmask=0077 0 2
-${DISK_DEVICE}${DISK_PARTITION_SEPARATOR}2 none swap sw 0 0
-${DISK_DEVICE}${DISK_PARTITION_SEPARATOR}3 / ext4 defaults 0 1
-EOF
-
-passwd <<EOF
-${PASSWORD}
-${PASSWORD}
-EOF
-
-echo "${SYSTEM_HOSTNAME}" >/etc/conf.d/hostname
 echo "${SYSTEM_HOSTNAME}" >/etc/hostname
 cat <<EOF >/etc/hosts
 # IPv4 and IPv6 localhost aliases
 127.0.0.1 ${SYSTEM_HOSTNAME} localhost
 ::1       ${SYSTEM_HOSTNAME} localhost
+EOF
+
+passwd <<EOF
+${PASSWORD}
+${PASSWORD}
 EOF
--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-LATEST_METADATA='https://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64-openrc.txt'
-LATEST_CHROOT_SCRIPT='https://raw.githubusercontent.com/pedro-pereira-dev/gentoo-installer/refs/heads/main/chroot.sh'
+LATEST_CHROOT_SCRIPT='https://raw.githubusercontent.com/pedro-pereira-dev/gentoo-installer/refs/heads/artix/chroot.sh'
 
 function usage() {
   echo -e "\nUsage: $(basename "$0")
@@ -35,7 +34,6 @@ if [[ -z ${PASSWORD} ]]; then
   done
 fi
 
-DISK_DEVICE=${DISK_DEVICE:-/dev/nvme0n1}
 DISK_PARTITION_SEPARATOR=$([[ "${DISK_DEVICE}" == *"nvme"* ]] && echo 'p' || echo '')
 
 EFI_SIZE=${EFI_SIZE:-'+1G'}
@@ -45,10 +43,6 @@ ROOT_SIZE=${ROOT_SIZE:-'" "'}
 TIMEZONE=${TIMEZONE:-'Europe/Lisbon'}
 KEYMAP=${KEYMAP:-'pt-latin9'}
 
-LATEST_RELEASE=$(wget --output-document=- --quiet "${LATEST_METADATA}")
-LATEST_BUILD=$(grep --only-matching --extended-regexp '.*/stage3-amd64-openrc-[0-9]*T[0-9]*Z.tar.xz' <<<"${LATEST_RELEASE}")
-LATEST_STAGE="https://distfiles.gentoo.org/releases/amd64/autobuilds/${LATEST_BUILD}"
-
 echo -e "
 Hostname: ${SYSTEM_HOSTNAME}
 Partitions: ${DISK_DEVICE}${DISK_PARTITION_SEPARATOR}...
@@ -57,13 +51,12 @@ SWAP size: ${SWAP_SIZE}
 ROOT size: ${ROOT_SIZE}
 Timezone: ${TIMEZONE}
 Keymap: ${KEYMAP}
-Stage3 file: ${LATEST_STAGE}
 
 All data from disk ${DISK_DEVICE} will be erased!"
 read -r -p 'Do you want to continue? (Y/n): ' CONFIRMATION && echo ''
 [[ ${CONFIRMATION} == 'n' || ${THIS_CONFIRMATION} == 'N' ]] && exit 0
 
-read -ra ALL_PARTITIONS <<<"$(blkid | grep -oE "${DISK_DEVICE}${DISK_PARTITION_SEPARATOR}\\w*" | xargs)"
+read -r -a ALL_PARTITIONS <<<"$(blkid | grep -oE "${DISK_DEVICE}${DISK_PARTITION_SEPARATOR}\\w*" | xargs)"
 yes | wipefs --all "${ALL_PARTITIONS[@]}" "${DISK_DEVICE}"
 sed --expression='s/\s*\([\+0-9a-zA-Z]*\).*/\1/' <<EOF | fdisk "${DISK_DEVICE}"
     g  # create empty GPT partition table
@@ -101,65 +94,18 @@ yes | mkfs.ext4 "${DISK_DEVICE}${DISK_PARTITION_SEPARATOR}3"      # ext4 ROOT
 swapon "${DISK_DEVICE}${DISK_PARTITION_SEPARATOR}2"
 mount --mkdir "${DISK_DEVICE}${DISK_PARTITION_SEPARATOR}3" /mnt
 mount --mkdir "${DISK_DEVICE}${DISK_PARTITION_SEPARATOR}1" /mnt/boot
-cd /mnt || exit 1
 
-chronyd -q
-wget "${LATEST_STAGE}" || exit 1
-tar fpx stage3-*.tar.xz --directory='/mnt' --numeric-owner --xattrs-include='*.*'
-
-rm --force --recursive /mnt/etc/portage/package.*
-touch /mnt/etc/portage/{package.accept_keywords,package.license,package.mask,package.use}
-cat <<'EOF' >/mnt/etc/portage/make.conf
-# architecture support
-COMMON_FLAGS="-march=native -O2 -pipe"
-RUSTFLAGS="${RUSTFLAGS} -C target-cpu=native"
-
-CFLAGS="${COMMON_FLAGS}"
-CXXFLAGS="${COMMON_FLAGS}"
-FCFLAGS="${COMMON_FLAGS}"
-FFLAGS="${COMMON_FLAGS}"
-
-# quiet data fetch
-FETCHCOMMAND="${FETCHCOMMAND} --quiet"
-RESUMECOMMAND="${RESUMECOMMAND} --quiet"
-
-GRUB_PLATFORMS="efi-64"
-LC_MESSAGES=C.utf8
-
-# binaries support
-FEATURES="${FEATURES} binpkg-request-signature getbinpkg"
-EMERGE_DEFAULT_OPTS="--ask --verbose --quiet"
-EOF
-
-AVAILABLE_RAM=$((($(free | awk '/Mem:/ {print $7}') / (1024 * 1024) + 1) / 2))
-AVAILABLE_THREADS=$(($(nproc) + 1))
-MAKE_OPTS_JOBS=$((AVAILABLE_RAM < AVAILABLE_THREADS ? AVAILABLE_RAM : AVAILABLE_THREADS))
-cat <<EOF >>/mnt/etc/portage/make.conf
-
-# WIP: other optimizations could be tmpfs of portage in zram, ccache and binhost
-# computed values based on ram and threads
-EMERGE_DEFAULT_OPTS="\${EMERGE_DEFAULT_OPTS} --jobs $(awk '{print int(($1 + 1) / 2)}' <<<${MAKE_OPTS_JOBS}) --load-average $(("${MAKE_OPTS_JOBS}" + 1))"
-MAKEOPTS="--jobs ${MAKE_OPTS_JOBS} --load-average $(("${MAKE_OPTS_JOBS}" + 1))"
-EOF
-
-cp --dereference /etc/resolv.conf /mnt/etc/
-mount --types proc /proc /mnt/proc
-mount --rbind /sys /mnt/sys
-mount --make-rslave /mnt/sys
-mount --rbind /dev /mnt/dev
-mount --make-rslave /mnt/dev
-mount --bind /run /mnt/run
-mount --make-slave /mnt/run
+dinitctl start ntpd
+basestrap /mnt base dinit efibootmgr elogind-dinit grub linux linux-firmware
+fstabgen -U /mnt >>/mnt/etc/fstab
 
 export DISK_DEVICE DISK_PARTITION_SEPARATOR TIMEZONE SYSTEM_HOSTNAME PASSWORD KEYMAP
 CHROOT_SCRIPT=$(mktemp)
 wget --output-document=- --quiet "${LATEST_CHROOT_SCRIPT}" >"${CHROOT_SCRIPT}" || exit 1
 { eval "echo \"$(sed 's/"/\\"/g')\""; } <"${CHROOT_SCRIPT}" >/mnt/chroot.sh
-rm --force "${CHROOT_SCRIPT}"
+rm "${CHROOT_SCRIPT}"
 
-chroot /mnt /bin/bash <<'EOF'
-env-update && source /etc/profile
+artix-chroot /mnt <<'EOF'
 source /chroot.sh
-rm --force /chroot.sh
-rm --force /stage3-*.tar.*
+rm /chroot.sh
 EOF

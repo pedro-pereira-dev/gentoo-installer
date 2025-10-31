f0fe09e6243c47c349efa07739456a464f9ee8ff
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2025-10-31 14:18:45 +0000

    feat: simplifies code and adds support for both aarch64 and amd64, as well as legacy and uefi boot mode

--- README.md
+++ README.md
@@ -1,21 +1,37 @@
 # Gentoo Installer
 
-Gentoo installation script for `amd64` with `openRC`. This script is intended to create a default gentoo base system with a distribution kernel and `doas` instead of `sudo`, as well as `networkmanager` for managing network interfaces.
+This script is used to obtain a [Gentoo](https://www.gentoo.org/) minimal installation on a systemm.
+Gentoo is a linux meta-distribution focused on user freedom of choice. As such it does not provide an easy installation method like other popular distributions, this project aims to fill in that gap for my own needs.
 
-### Disclaimer
+*I use this project to setup and maintain all my Gentoo systems and is not intended for community use.*
 
-This project can be used as reference for different solutions but is not the final and ultimate approach for every use case. Any contribution either in the form of issues discovering, pull requests or ideas are welcome.
+The project supports both *`aarch64`* and *`amd64`* CPU architectures, and both *`BIOS / MBR legacy`* and *`UEFI`* boot mode.
+It installs common utilities and Gentoo's distribution kernel.
+Additional packages and customizations are implemented in the target systems with custom scripts by my own [_dotfiles_](https://github.com/pedro-pereira-dev/dotfiles) management system.
 
-## Install
+## Usage
 
-To install gentoo using this project, boot up a live ISO system and run the following command to download this installation script and run it with `bash` from an elevated shell session.
+This script is _POSIX_ compliant so it can be executed with any compatible shell and expects that disk partioning is performed beforehand by accepting a target boot and root filesystem device partition.
+If no arguments are provided to the script, simple interactive questions are displayed to gather the required information for the installation.
 
-This script was tested only with Ubuntu but will most likely work (famous last words) with any of the major Linux distributions as dependencies are quite common, such as `wget`, `tar`, etc.
+### Basic usage
 
-```bash
-bash <(wget -qO- https://raw.githubusercontent.com/pedro-pereira-dev/gentoo-installer/refs/heads/main/install.sh) \
-  --hostname 'host' --username 'user'                                                                             \
-  --device 'nvme0n1' --device-separator 'p'                                                                       \
-  --efi-size '+1G' --swap-size '+32G' --root-size '+64G'                                                          \
-  --timezone 'Europe/Lisbon' --keymap 'pt-latin9'
+```sh
+_TMP_FILE=$(mktemp)
+curl -Lfs -o "$_TMP_FILE" 'https://raw.githubusercontent.com/pedro-pereira-dev/gentoo-installer/refs/heads/main/install.sh'
+sh "$_TMP_FILE"
+```
+
+### Unattended usage
+
+```sh
+_TMP_FILE=$(mktemp)
+curl -Lfs -o "$_TMP_FILE" 'https://raw.githubusercontent.com/pedro-pereira-dev/gentoo-installer/refs/heads/main/install.sh'
+sh "$_TMP_FILE" \
+  --hostname "$_HOSTNAME" \
+  --password "$_PASSWORD" \
+  --boot "$_BOOT_DEV" \
+  --root "$_ROOT_DEV" \
+  --keymap 'pt-latin9' \
+  --timezone 'Europe/Lisbon'
 ```
--- install.sh
+++ install.sh
@@ -1,165 +1,163 @@
-#!/bin/bash
+#!/bin/sh
+# shellcheck disable=SC2016
 
-# checks arguments, prints usage and prepares variables
-while [[ $# -gt 0 ]]; do
-  case $1 in
-  -h | --help) usage && exit 0 ;;
-  --hostname) THIS_HOST=$2 ;; --username) THIS_USER=$2 ;;
-  --device) THIS_DEVICE=$2 ;; --device-separator) THIS_DEVICE_SEPARATOR=$2 ;;
-  --efi-size) THIS_EFI_SIZE=$2 ;; --swap-size) THIS_SWAP_SIZE=$2 ;; --root-size) THIS_ROOT_SIZE=$2 ;;
-  --timezone) THIS_TIMEZONE=$2 ;; --keymap) THIS_KEYMAP=$2 ;;
+is_aarch64() { test "$(uname -m)" = 'aarch64'; }
+is_amd64() { test "$(uname -m)" = 'x86_64'; }
+
+is_bios() { ! is_uefi; }
+is_uefi() { test -d '/sys/firmware/efi'; }
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --hostname) _HOSTNAME=$2 ;; --password) _PASSWORD=$2 ;;
+  --boot) _BOOT_DEV=$2 ;; --root) _ROOT_DEV=$2 ;;
+  --keymap) _KEYMAP=$2 ;; --timezone) _TIMEZONE=$2 ;;
   esac
-  shift
-  shift
+  shift && shift
 done
-if [[ -z ${THIS_HOST} || -z ${THIS_USER} ]]; then
-  echo -e "Usage: $0 --hostname <hostname> --username <username>
-          [--device <device>] [--device-separator <separator>]
-          [--efi-size <size>] [--swap-size <size>] [--root-size <size>]
-          [--timezone <timezone>] [--keymap <keymap>]"
-  exit 1
-fi
 
-# reads password
-echo 'Setting up system password:'
-read -s -p ' - Password: ' THIS_PASSWORD && echo ''
-read -s -p ' - Confirm password: ' THIS_PASSWORD_CONFIRMATION && echo ''
-# checks that passwords match
-[[ ${THIS_PASSWORD} != ${THIS_PASSWORD_CONFIRMATION} ]] && echo 'Passwords do not match' && exit 1
+[ -z "$_HOSTNAME" ] && while true; do
+  printf 'Hostname: ' && read -r _HOSTNAME
+  [ -n "$_HOSTNAME" ] && case "$_HOSTNAME" in
+  *[!a-zA-Z0-9-]* | '') ;;
+  *) break ;;
+  esac
+done
+[ -z "$_PASSWORD" ] && while true; do
+  printf 'Password: ' && read -r _PASSWORD
+  [ -z "$_PASSWORD" ] && continue
+  printf 'Confirm password: ' && read -r _PASSWORD_CONFIRMATION
+  [ "$_PASSWORD" = "$_PASSWORD_CONFIRMATION" ] && break
+done
 
-# sets default values
-THIS_DEVICE=/dev/${THIS_DEVICE:-nvme0n1}
-THIS_EFI_SIZE=${THIS_EFI_SIZE:-+1G}
-THIS_SWAP_SIZE=${THIS_SWAP_SIZE:-+32G}
-THIS_ROOT_SIZE=${THIS_ROOT_SIZE:-+64G}
-THIS_TIMEZONE=${THIS_TIMEZONE:-Europe/Lisbon}
-THIS_KEYMAP=${THIS_KEYMAP:-pt-latin9}
+[ -z "$_BOOT_DEV" ] && while true; do
+  printf 'Boot device:' && read -r _BOOT_DEV
+  [ -e "$_BOOT_DEV" ] && break
+done
+[ -z "$_ROOT_DEV" ] && while true; do
+  printf 'Root device:' && read -r _ROOT_DEV
+  [ -e "$_ROOT_DEV" ] && break
+done
 
-# gets current stage3 url
-STAGE_BUILD=$(wget -qO- https://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64-openrc.txt |
-  grep -oE '.*/stage3-amd64-openrc-[0-9]*T[0-9]*Z.tar.xz')
-THIS_STAGE_FILE="https://distfiles.gentoo.org/releases/amd64/autobuilds/${STAGE_BUILD}"
+[ -z "$_KEYMAP" ] &&
+  printf 'Keymap: [pt-latin9] ' && read -r _KEYMAP
+_KEYMAP=${_KEYMAP:-'pt-latin9'}
+[ -z "$_TIMEZONE" ] &&
+  printf 'Timezone: [Europe/Lisbon] ' && read -r _TIMEZONE
+_TIMEZONE=${_TIMEZONE:-'Europe/Lisbon'}
 
-# checks user confirmation
-echo -e "
-Hostname: ${THIS_HOST}
-Username: ${THIS_USER}
-Password: ********
-Device: ${THIS_DEVICE}
-Device separator: ${THIS_DEVICE_SEPARATOR}
-EFI size: ${THIS_EFI_SIZE}
-SWAP size: ${THIS_SWAP_SIZE}
-ROOT size: ${THIS_ROOT_SIZE}
-Timezone: ${THIS_TIMEZONE}
-Keymap: ${THIS_KEYMAP}
-Stage3 file: ${THIS_STAGE_FILE}
-"
-read -p 'Do you want to continue? (Y/n): ' THIS_CONFIRMATION && echo ''
-[[ ${THIS_CONFIRMATION} == 'n' || ${THIS_CONFIRMATION} == 'N' ]] && exit 0
+echo 'Installation details:'
+is_bios && echo ' - System partition table: msdos'
+is_uefi && echo ' - System partition table: gpt'
+echo " - System architecture: $(uname -m)"
+echo " - System hostname: $_HOSTNAME"
+echo " - System password: $_PASSWORD"
+echo " - System boot device: $_BOOT_DEV"
+echo " - System root device: $_ROOT_DEV"
+echo " - System keymap: $_KEYMAP"
+echo " - System timezone: $_TIMEZONE"
+echo ''
+echo "All data from devices $_BOOT_DEV and $_ROOT_DEV will be erased!"
+printf 'Press any key to continue...' && read -r _
 
-# partitions disk, a blank line will send a return to fdisk
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' <<EOF | fdisk ${THIS_DEVICE}
-    g  # create empty GPT partition table
-    n  # create EFI partition
-    # choose default partition number
-    # choose default sector number
-    ${THIS_EFI_SIZE}
-    t  # create EFI partition type
-    1  # EFI system type
-    n  # create SWAP partition
-    # choose default partition number
-    # choose default sector number
-    ${THIS_SWAP_SIZE}
-    t  # create SWAP partition type
-    # choose default partition number
-    19 # Linux swap type
-    n  # create ROOT partition
-    # choose default partition number
-    # choose default sector number
-    ${THIS_ROOT_SIZE}
-    t  # create ROOT partition type
-    # choose default partition number
-    23 # Linux root (x86-64) type
-    p  # print partition table
-    w  # write changes to disk
-EOF
+is_bios && yes | mkfs.ext4 "$_BOOT_DEV"      # mbr / bios partition with EXT4
+is_uefi && yes | mkfs.fat -F 32 "$_BOOT_DEV" # efi partition with FAT32
+yes | mkfs.ext4 "$_ROOT_DEV"                 # root partition with EXT4
 
-# creates filesystems
-mkfs.vfat -F 32 ${THIS_DEVICE}${THIS_DEVICE_SEPARATOR}1 # fat32 BOOT
-swapon ${THIS_DEVICE}${THIS_DEVICE_SEPARATOR}2          # activates swap
-mkfs.ext4 ${THIS_DEVICE}${THIS_DEVICE_SEPARATOR}3 <<EOF
-y
-EOF
+is_bios && _BOOT_MOUNT='/boot'
+is_uefi && _BOOT_MOUNT='/efi'
 
-# mounts root partition
-mkdir -p /mnt/gentoo && mount ${THIS_DEVICE}${THIS_DEVICE_SEPARATOR}3 /mnt/gentoo
-cd /mnt/gentoo
+mount -m "$_ROOT_DEV" /mnt
+mount -m "$_BOOT_DEV" "/mnt$_BOOT_MOUNT"
 
-# downloads stage file
-wget ${THIS_STAGE_FILE} || exit 1
-tar xpf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
+is_aarch64 && _ARCH='arm64'
+is_amd64 && _ARCH='amd64'
+_METADATA="https://gentoo.osuosl.org/releases/$_ARCH/autobuilds/latest-stage3-$_ARCH-openrc.txt"
+_BUILD=$(curl -Lfs "$_METADATA" | sed -n '6p' | cut -d' ' -f1)
+_STAGE_FILE="https://distfiles.gentoo.org/releases/$_ARCH/autobuilds/$_BUILD"
 
-# configures default portage configuration
-rm -rf /mnt/gentoo/etc/portage/package.*
-touch /mnt/gentoo/etc/portage/{package.license,package.use}
-cat <<EOF >/mnt/gentoo/etc/portage/make.conf
-# global USE flags
-USE="dbus"
+echo 'Dowloading latest gentoo stage release...'
+curl -Lf --progress-bar -o /mnt/stage3-current.tar.xz "$_STAGE_FILE"
+tar xpf /mnt/stage3-current.tar.xz -C /mnt --numeric-owner --xattrs-include='*.*'
+rm -fr /mnt/etc/portage/package.*
 
-# CPU settings
-CPU_FLAGS_X86=""
-# GPU settings
-VIDEO_CARDS=""
-# Input settings
-INPUT_DEVICES=""
+cp -L /etc/resolv.conf /mnt/etc/
+mount --types proc /proc /mnt/proc
+mount --rbind /sys /mnt/sys
+mount --make-rslave /mnt/sys
+mount --rbind /dev /mnt/dev
+mount --make-rslave /mnt/dev
+mount --bind /run /mnt/run
+mount --make-slave /mnt/run
 
-# portage default options
-#MAKEOPTS="-jXX -lYY"
-EMERGE_DEFAULT_OPTS="--ask --verbose --quiet-build"
-FEATURES="getbinpkg binpkg-request-signature"
+_THREAD_RAM=$(($(free -g | awk '/Mem:/ {print $2}') / 2))       # RAM in GB divided by 2GB
+_THREADS=$(nproc)                                               # number of threads
+_MAKE_JOBS=$((_THREAD_RAM < _THREADS ? _THREAD_RAM : _THREADS)) # min(RAM / 2GB, number of threads)
+_MAKE_JOBS=$((_MAKE_JOBS > 1 ? _MAKE_JOBS : 1))                 # max(make_opt_jobs, 1)
+_LOAD_JOBS=$((_MAKE_JOBS + 1))                                  # max number of jobs plus one for io
+_PORTAGE_JOBS=$(((_MAKE_JOBS + 1) / 2))                         # ceiling of half max number of jobs
 
-# compiler settings
-COMMON_FLAGS="-march=native -O2 -pipe"
-CFLAGS="\${COMMON_FLAGS}"
-CXXFLAGS="\${COMMON_FLAGS}"
-FCFLAGS="\${COMMON_FLAGS}"
-FFLAGS="\${COMMON_FLAGS}"
+is_bios && _PLATFORM='pc'
+is_uefi && _PLATFORM='efi-64'
+mkdir -p /mnt/etc/portage/env /mnt/etc/portage/package.env
+echo '*/* 0-gentoo-installer-make.conf' >>/mnt/etc/portage/package.env/0-gentoo-installer-env.conf
+{
+  echo '# values modified by the installation script'
+  echo 'COMMON_FLAGS="-march=native -O2 -pipe"'
+  echo 'RUSTFLAGS="$RUSTFLAGS -C target-cpu=native"'
+  echo 'CFLAGS="$COMMON_FLAGS"'
+  echo 'CXXFLAGS="$COMMON_FLAGS"'
+  echo 'FCFLAGS="$COMMON_FLAGS"'
+  echo 'FFLAGS="$COMMON_FLAGS"'
+  echo ''
+  echo '# quiet fetches'
+  echo 'FETCHCOMMAND="$FETCHCOMMAND -q"'
+  echo 'RESUMECOMMAND="$RESUMECOMMAND -q"'
+  echo ''
+  echo '# bootloader platform architecture'
+  echo "GRUB_PLATFORMS=\"$_PLATFORM\""
+  echo ''
+  echo '# portage default options'
+  echo "EMERGE_DEFAULT_OPTS=\"--ask --jobs $_PORTAGE_JOBS --load-average $_LOAD_JOBS --quiet --verbose\""
+  echo "FEATURES=\"$FEATURES binpkg-request-signature getbinpkg\""
+  echo "MAKEOPTS=\"--jobs $_MAKE_JOBS --load-average $_LOAD_JOBS\""
+} >>/mnt/etc/portage/env/0-gentoo-installer-make.conf
 
-# GRUB EFI settings
-GRUB_PLATFORMS="efi-64"
-# default build output language
-LC_MESSAGES=C.utf8
-EOF
+chroot /mnt /bin/bash -c 'emerge-webrsync'
+chroot /mnt /bin/bash -c "ln -fs '/usr/share/zoneinfo/$_TIMEZONE' /etc/localtime"
+chroot /mnt /bin/bash -c 'hwclock -uw'
+chroot /mnt /bin/bash -c 'echo "en_US.UTF-8 UTF-8" >/etc/locale.gen'
+chroot /mnt /bin/bash -c 'locale-gen && eselect locale set 4'
+chroot /mnt /bin/bash -c "sed -i 's/keymap=\"[^\"]*\"*/keymap=\"$_KEYMAP\"/g' /etc/conf.d/keymaps"
+chroot /mnt /bin/bash -c 'env-update'
 
-# mounts system
-cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
-mount --types proc /proc /mnt/gentoo/proc
-mount --rbind /sys /mnt/gentoo/sys
-mount --make-rslave /mnt/gentoo/sys
-mount --rbind /dev /mnt/gentoo/dev
-mount --make-rslave /mnt/gentoo/dev
-mount --bind /run /mnt/gentoo/run
-mount --make-slave /mnt/gentoo/run
-test -L /dev/shm && rm /dev/shm && mkdir /dev/shm
-mount --types tmpfs --options nosuid,nodev,noexec shm /dev/shm
-chmod 1777 /dev/shm /run/shm
+mkdir -p /mnt/etc/portage/package.license /mnt/etc/portage/package.use
+{
+  echo '#!/bin/sh'
+  echo 'sys-kernel/linux-firmware @BINARY-REDISTRIBUTABLE'
+} >>/mnt/etc/portage/package.license/0-gentoo-installer-license.conf
+{
+  echo '#!/bin/sh'
+  echo 'sys-kernel/installkernel dracut grub'
+} >>/mnt/etc/portage/package.use/0-gentoo-installer-use.conf
+chroot /mnt /bin/bash -c 'emerge --ask=n sys-kernel/gentoo-kernel-bin sys-kernel/installkernel sys-kernel/linux-firmware'
+chroot /mnt /bin/bash -c 'eselect news read --quiet all'
 
-# exports all variables
-export THIS_HOST THIS_USER THIS_PASSWORD THIS_DEVICE THIS_DEVICE_SEPARATOR THIS_TIMEZONE THIS_KEYMAP
-# creates chroot script from wget
-THIS_CHROOT_SCRIPT=$(mktemp)
-wget -qO- https://raw.githubusercontent.com/pedro-pereira-dev/gentoo-installer/refs/heads/main/chroot.sh >${THIS_CHROOT_SCRIPT} || exit 1
-envsubst <${THIS_CHROOT_SCRIPT} >/mnt/gentoo/chroot.sh
-rm -f ${THIS_CHROOT_SCRIPT}
+is_bios && _GRUB_INSTALL="/dev/$(lsblk -dno pkname "$_BOOT_DEV")"
+is_uefi && _GRUB_INSTALL='--efi-directory=/efi --removable'
 
-# chroots into system
-chroot /mnt/gentoo /bin/bash <<EOF
-source /etc/profile && export PS1="(chroot) \${PS1}"
-source /chroot.sh
+chroot /mnt /bin/bash -c "grub-install $_GRUB_INSTALL"
+chroot /mnt /bin/bash -c 'grub-mkconfig -o /boot/grub/grub.cfg'
 
-eselect news read 
-emerge --ask=n --depclean
+{
+  echo '# <fs> <mountpoint> <type> <opts> <dump> <pass>'
+  is_bios && echo "$_BOOT_DEV $_BOOT_MOUNT ext4 defaults,noatime,nodev,nosuid 0 2"
+  is_uefi && echo "$_BOOT_DEV $_BOOT_MOUNT vfat defaults,noatime,nodev,noexec,nosuid,umask=0077 0 2"
+  echo "$_ROOT_DEV / ext4 defaults,noatime 0 1"
+} >/mnt/etc/fstab
 
-rm /stage3-*.tar.*
-rm /chroot.sh
-EOF
+echo "$_HOSTNAME" >/mnt/etc/hostname
+sed -i "s/hostname=\"[^\"]*\"/hostname=\"$_HOSTNAME\"/g" /mnt/etc/conf.d/hostname
+echo "root:$_PASSWORD" | chroot /mnt /usr/sbin/chpasswd
+
+rm -f /mnt/stage3-current.tar.xz
